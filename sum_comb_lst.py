b040e41dfb1a16105c45df9b07c9e1633f54e23b
viktorfad <66686633+viktorfad@users.noreply.github.com>
2020-07-01 17:04:49 +0300

    Update sum_comb_lst.py

--- sum_comb_lst.py
+++ sum_comb_lst.py
@@ -1,23 +1,23 @@
 from itertools import combinations
 from errors import *
 
-def search_pairs(array,k):
-    pair=[]
+def search_pairs(array, k):
+    pair = []
     try:
         for el in array:
-            if isinstance(el,int)== False:
-                raise ArrayTypeError('Ошибка! \nЭлементы массива array должны являться числами типа int.\nВы передали {} - {}'.format(el,type(el)))
-        if isinstance(k,int)==False:
+            if isinstance(el, int) == False:
+                raise ArrayTypeError('Ошибка! \nЭлементы массива array должны являться числами типа int.\nВы передали {} - {}'.format(el, type(el)))
+        if isinstance(k, int) == False:
             raise kTypeError('Ошибка! \nАргумент k должен являться числом типа int. \nВы передали {}.'.format(type(k)))
-        for item in combinations(array, 2): 
-            if sum(item)==k:
+        for item in combinations(array, 2):
+            if sum(item) == k:
                 if item not in pair and tuple(reversed(item)) not in pair:
-                    pair.append(item)
-        if pair==[]:
+                            pair.append(item)
+        if pair == []:
             raise NullValue('Не найдено пар чисел, сумма которых равна числу {}'.format(k))
         return pair
     except TypeError as t:
-        print('Ошибка! \nФункция search_pairs(array,k)(где array - массив, k - целое число) получила неверные данные: array - {}, k - {}. \nТакже массив должен состоять из целых чисел!\n{}'.format(type(array),type(k),t))
+        print('Ошибка! \nФункция search_pairs(array,k)(где array - массив, k - целое число) получила неверные данные: array - {}, k - {}. \nТакже массив должен состоять из целых чисел!\n{}'.format(type(array), type(k), t))
         return
     except kTypeError as k:
         print(k)
@@ -28,5 +28,3 @@ def search_pairs(array,k):
     except ArrayTypeError as a:
         print(a)
         return
-    
-    
